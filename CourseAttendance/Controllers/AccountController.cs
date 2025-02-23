ddfb9d48bd0e06993822d704c8b1ee5c95a40485
tingzhong <tingzhong666@gmail.com>
2025-02-23 20:40:20 +0800

    -

---
+++ CourseAttendance/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using CourseAttendance.Repositories.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseAttendance.Controllers
+{
+	[Route("api/account")]
+	[ApiController]
+	public class AccountController: ControllerBase
+	{
+		private readonly UserManager<UserRepository> _userManager;
+		public AccountController(UserManager<UserRepository> userManager)
+		{
+			_userManager = userManager;
+		}
+	}
+}
